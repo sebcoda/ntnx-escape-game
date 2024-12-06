1dab7f2448e1daa02187cd1e2da1733ddbce6393
Gautier LEBLANC <gautier.leblanc@nutanix.com>
2024-12-06 23:58:23 +0100

    fixed mail script

--- mail.py
+++ mail.py
@@ -3,12 +3,13 @@ import sys
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-if len(sys.argv) != 3:
-    print("Usage: mail.py <recipient email> <template name>")
+if len(sys.argv) != 4:
+    print("Usage: mail.py <recipient email> <title> <template name>")
     exit(1)
 else:
     recipient = sys.argv[1]
-    templateName=sys.argv[2]
+    title = sys.argv[2]
+    templateName=sys.argv[3]
 
 file = open("email_templates/"+templateName, "r")
 html_content = file.read()
@@ -16,7 +17,7 @@ html_content = file.read()
 message = Mail(
     from_email="Your new important friend <gautier.leblanc@nutanix.com>",
     to_emails= recipient,
-    subject='I need help right now!',
+    subject=title,
     html_content=html_content
 )
 try:
